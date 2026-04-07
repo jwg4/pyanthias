f8261ee7695dcecff0730e2df41828176928e650
Jack Grahl <jack.grahl@gmail.com>
2026-04-07 02:46:11 +0100

    Break out transformation by id.

--- src/anthias/basic.py
+++ src/anthias/basic.py
@@ -23,13 +23,17 @@ def find_id_by_name(name, root_url):
     raise ValueError(f"Could not find an asset with name '{name}'")
 
 
+def write_enabled_by_id(id_, enabled, root_url):
+    url = f"{root_url}/assets/{id_}"
+    r = requests.patch(url, json={"is_enabled": 1 if enabled else 0})
+    r.raise_for_status()
+
+
 def write_enabled(name, enabled):
     root_url = get_root_url()
     id_ = find_id_by_name(name, root_url)
     
-    url = f"{root_url}/assets/{id_}"
-    r = requests.patch(url, json={"is_enabled": 1 if enabled else 0})
-    r.raise_for_status()
+    write_enabled_by_id(id_, enabled, root_url)
 
 
 def enable(name):
