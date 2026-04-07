5c0d8147f81c832d7c4c2163de4852065954354a
Jack Grahl <jack.grahl@gmail.com>
2026-04-07 02:47:25 +0100

    Add function to work on IDs.

--- src/anthias/basic.py
+++ src/anthias/basic.py
@@ -29,6 +29,16 @@ def write_enabled_by_id(id_, enabled, root_url):
     r.raise_for_status()
 
 
+def enable_asset(id_):
+    root_url = get_root_url()
+    write_enabled_by_id(id_, True, root_url)
+
+
+def disable_asset(id_):
+    root_url = get_root_url()
+    write_enabled_by_id(id_, False, root_url)
+
+
 def write_enabled(name, enabled):
     root_url = get_root_url()
     id_ = find_id_by_name(name, root_url)
